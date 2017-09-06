b1b82386c6b95b56bfcc9346c2a8cae8126571c9
neilsatt <neilsatt@gmail.com>
2017-09-05 20:35:33 -0600

    updates to styling transitions

--- nic_review/returnObjectFunction.js
+++ nic_review/returnObjectFunction.js
@@ -5,3 +5,53 @@
     2) Multiple the last item in the array by the number
     
     3) Return an object that logs out the result
+    
+*/
+
+// Version One
+
+var player = makeGamePlayer("John Smith", 15, 3);
+The code above calls function makeGamePlayer and stores the returned value in the variable player. In this case, you may want to define the function like this:
+
+function makeGamePlayer(name, totalScore, gamesPlayed) {
+  // Define desired object
+  var obj = {
+    name:  name,
+    totalScore: totalScore,
+    gamesPlayed: gamesPlayed
+  };
+  // Return it
+  return obj;
+}
+
+
+
+// Version Two
+
+    function makeGamePlayer(name,totalScore,gamesPlayed) {
+    return {
+        name: name,
+        totalscore: totalScore,
+        gamesPlayed: gamesPlayed
+     };
+    }
+
+
+
+
+// Version Three
+
+  function makeGamePlayer(name,totalScore,gamesPlayed) {
+        //should return an object with three keys:
+        // name
+        // totalScore
+        // gamesPlayed
+
+         var obj = {  //note you don't use = in an object definition
+             "name": name,
+             "totalScore": totalScore,
+             "gamesPlayed": gamesPlayed
+          }
+         return obj;
+    }
+    
