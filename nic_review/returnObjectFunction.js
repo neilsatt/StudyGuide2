675c5daeae8636b99de356961af974eda208f38f
neilsatt <neilsatt@gmail.com>
2017-09-17 19:56:43 -0600

    clean up

--- nic_review/returnObjectFunction.js
+++ nic_review/returnObjectFunction.js
@@ -1,43 +1,31 @@
 /* 
-
-    Create a function that takes in an array and a number
-    1) Multiple the first item in the array by the number
-    2) Multiple the last item in the array by the number
-    
-    3) Return an object that logs out the result
-    
-    https://stackoverflow.com/questions/12272239/javascript-function-returning-an-object
-    
+   - Create a function that accepts an array and number as its parameters
+   - Multiply the first number of the array by the number
+   - Multiply the last number of the array by the number
+   - Create and object that shows the two calculations, and print out the result
+   	 using console.log
 */
-
+   
 function myFunction(myArray, num) {
-	var firstArrayNum = myArray[0];
-    var lastArrayNum = myArray.pop(); 
-    //Test
+	var firstArrayNum = myArray[0]; // stores first number
+    var lastArrrayNum = myArray[myArray.length-1]; // stores last number 
+
 	console.log(firstArrayNum);
-	console.log(lastArrayNum);
+    console.log(lastArrrayNum);
     
-    var calculationOne = firstArrayNum * num;
-    var calculationTwo = lastArrayNum * num;
-    //Test two
-	console.log(calculationOne);
-	console.log(calculationTwo);
+    var firstCalculation = firstArrayNum * num;
+    var secondCalculation = lastArrrayNum * num;
     
-      var obj = {
-    	firstCalculationTotal:  'First : '+calculationOne,
-    	secondCalculationTotal: 'Second: ' +calculationTwo
-  	};
-  // Return it
-  return obj;
-      
+   var answerObj = {
+    firstCalculation: 'First Calculation answer is: '+firstCalculation,
+    secondCalculation: 'Second Calculation answer is: '+ secondCalculation,
+  };
+    
+   for(var prop in answerObj) {
+    console.log(answerObj[prop]);
+    //console.log(prop + ": " +answerObj[prop]);
+   }
 }
 
-var returnValue = myFunction([34,55,32], 7);
-console.log(returnValue)
-
-/* Another way to get the last item in an array, without removing it */
-
-var myArray = [2,5,3,7,9,56,32];
-
-var lastArrayItem = myArray[myArray.length - 1];
+myFunction([5,10,3], 5);
     
